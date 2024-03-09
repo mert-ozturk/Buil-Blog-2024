d9add03794875fd9f8802526e2b6628b9620148e
mert-ozturk <mertozturk.seo.yonetim@gmail.com>
2024-03-09 15:17:39 +0300

    islem1

---
+++ app/components/Footer.tsx
@@ -0,0 +1,7 @@
+
+
+export default function Footer () {
+  return <footer className="py-3">This website is desing by Mert © 2024</footer>
+    
+  
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,34 @@
+import { postsData } from "@/data";
+import Post from "../components/Post";
+import Link from "next/link";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+
+export default async function Dashborad() {
+  const session = await getServerSession(authOptions)
+  if(!session) {
+    redirect("/sign-in")
+  }
+    return (
+        <div>
+            <h1>My Post</h1>
+
+
+            {postsData && postsData.length > 0 ? (
+            postsData.map(post => (<Post key={post.id} id={post.id} author={post.author}
+            authorEmail={'test@email.com'} date={post.datepublished} thumbnail={post.thumbnail}
+            category={post.category} title={post.title} content={post.content}
+            links={post.links ||[]}
+            />)) 
+          ) : (
+            <div className="py-6">
+                No post created yet. <Link className="underline" href={'/create-post'}>Create New</Link>
+            
+            </div>
+          )}
+        </div>
+
+        
+    )
+}
