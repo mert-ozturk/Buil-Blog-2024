34d7fe65162a23b72a951bef349d18fff5c2690a
mert-ozturk <mertozturk.seo.yonetim@gmail.com>
2024-03-10 00:19:01 +0300

    islem3

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,8 @@
 import NextAuth, { AuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
-import { PrismaAdapter } from "@auth/prisma-adapter"
- import prisma from "@/lib/prismadb";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "@/lib/prismadb";
 
 
 export const authOptions: AuthOptions = {
